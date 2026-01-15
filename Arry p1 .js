154e88680515cb1ebf419f0767609c7a9baa97aa
Sahal-Ahmad7 <sahalahmad051@gmail.com>
2026-01-15 09:37:20 +0500

    Arrays in JavaScript

---
+++ Arry p1 .js
@@ -0,0 +1,19 @@
+
+let myArr = ["sahal" , 3 ,  4 , true]
+console.log(myArr);
+
+//**************Arry Methods *********************************/
+
+myArr.push(7)
+console.log(myArr);
+
+myArr.pop()
+console.log(myArr);
+
+myArr.shift(11)
+console.log(myArr);
+
+myArr.unshift(10)
+console.log(myArr);
+
+console.log(myArr.includes(11));

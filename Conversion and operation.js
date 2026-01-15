b661773f3bcd66fdca0c502b9a1a2df64b19e98d
Sahal-Ahmad7 <sahalahmad051@gmail.com>
2026-01-15 09:39:27 +0500

    Operations

---
+++ Conversion and operation.js
@@ -0,0 +1,13 @@
+let a = 33
+let aintos = String(a)
+//console.log(aintos);
+//console.log(typeof aintos);
+
+
+///////////********** OPERATION **********************/
+
+let str1 = "hi "
+let str2 = "sazmic"
+
+//console.log(str1 + str2);
+console.log(+true);
